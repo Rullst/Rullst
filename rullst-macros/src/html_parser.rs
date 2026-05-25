907d687cb8e8c4daf6456e82c00d992e6b84df5e
Venelouis <venelouistyago@gmail.com>
2026-05-25 03:39:43 -0300

    chore: release version 0.6.0 with enterprise features

--- rullst-macros/src/html_parser.rs
+++ rullst-macros/src/html_parser.rs
@@ -1,5 +1,5 @@
 use proc_macro2::TokenStream;
-use quote::{quote, ToTokens};
+use quote::quote;
 use syn::parse::{Parse, ParseStream};
 use syn::ext::IdentExt;
 use syn::{token, Expr, Ident, LitStr, Result, Token};
