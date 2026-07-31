ee1a69398e95528ab8f3460388e6d9ea53c13981
github-action-benchmark <github@users.noreply.github.com>
2026-07-31 19:21:28 +0000

    add Rullst Auth Benchmark (cargo) benchmark result for b87bb872b01466e5f5be401e32e8d4dc281570a1

--- benches-auth/data.js
+++ benches-auth/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1785524640772,
+  "lastUpdate": 1785525688648,
   "repoUrl": "https://github.com/Rullst/Rullst",
   "entries": {
     "Rullst Auth Benchmark": [
@@ -527,6 +527,54 @@ window.BENCHMARK_DATA = {
             "name": "make_login_cookie",
             "value": 1100,
             "range": "± 56",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "venelouistyago@gmail.com",
+            "name": "venelouis",
+            "username": "venelouis"
+          },
+          "committer": {
+            "email": "venelouistyago@gmail.com",
+            "name": "venelouis",
+            "username": "venelouis"
+          },
+          "distinct": true,
+          "id": "b87bb872b01466e5f5be401e32e8d4dc281570a1",
+          "message": "feat: add UI components including update checker, interactive spinner, and CLI dashboard",
+          "timestamp": "2026-07-31T16:14:11-03:00",
+          "tree_id": "699b80b2b65295ab36a8af6847c00202c5910cfd",
+          "url": "https://github.com/Rullst/Rullst/commit/b87bb872b01466e5f5be401e32e8d4dc281570a1"
+        },
+        "date": 1785525688023,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "session_crypto/encrypt_session",
+            "value": 721,
+            "range": "± 5",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "session_crypto/decrypt_session",
+            "value": 624,
+            "range": "± 11",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "session_crypto/round_trip_encrypt_decrypt",
+            "value": 1367,
+            "range": "± 11",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "make_login_cookie",
+            "value": 1371,
+            "range": "± 13",
             "unit": "ns/iter"
           }
         ]
