6dba264afd50883e90a051c74774cbdb260cbb49
github-action-benchmark <github@users.noreply.github.com>
2026-08-04 03:03:35 +0000

    add Rullst Connect Benchmark (cargo) benchmark result for b7140b7e339b68c1857a177b9e9771cbe129f21f

--- benches-connect/data.js
+++ benches-connect/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1785807610370,
+  "lastUpdate": 1785812615073,
   "repoUrl": "https://github.com/Rullst/Rullst",
   "entries": {
     "Rullst Connect Benchmark": [
@@ -868,6 +868,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "github_provider_creation",
             "value": 70,
+            "range": "± 0",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "venelouistyago@gmail.com",
+            "name": "Venelouis",
+            "username": "venelouis"
+          },
+          "committer": {
+            "email": "venelouistyago@gmail.com",
+            "name": "Venelouis",
+            "username": "venelouis"
+          },
+          "distinct": true,
+          "id": "b7140b7e339b68c1857a177b9e9771cbe129f21f",
+          "message": "refactor: apply consistent code formatting and style improvements across nexus, orm, and studio modules",
+          "timestamp": "2026-08-03T23:55:23-03:00",
+          "tree_id": "8ab4d7700f816fa28072773137a99bac0c019e92",
+          "url": "https://github.com/Rullst/Rullst/commit/b7140b7e339b68c1857a177b9e9771cbe129f21f"
+        },
+        "date": 1785812614796,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "github_provider_creation",
+            "value": 75,
             "range": "± 0",
             "unit": "ns/iter"
           }
