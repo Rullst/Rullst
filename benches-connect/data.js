c0433f4f62484978e32f906ff80f84a812682abd
github-action-benchmark <github@users.noreply.github.com>
2026-08-05 20:08:55 +0000

    add Rullst Connect Benchmark (cargo) benchmark result for 7004aecbf98363bc245a29379c68e91cb97a925e

--- benches-connect/data.js
+++ benches-connect/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1785871435036,
+  "lastUpdate": 1785960535634,
   "repoUrl": "https://github.com/Rullst/Rullst",
   "entries": {
     "Rullst Connect Benchmark": [
@@ -1043,6 +1043,36 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/Rullst/Rullst/commit/f3e8ae62e1da83078163ba65b39558d0fc65498d"
         },
         "date": 1785871434244,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "github_provider_creation",
+            "value": 72,
+            "range": "± 0",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "venelouistyago@gmail.com",
+            "name": "Venelouis",
+            "username": "venelouis"
+          },
+          "committer": {
+            "email": "venelouistyago@gmail.com",
+            "name": "Venelouis",
+            "username": "venelouis"
+          },
+          "distinct": true,
+          "id": "7004aecbf98363bc245a29379c68e91cb97a925e",
+          "message": "feat: implement major framework expansion with k8s/Paas deployment, gRPC support, dependency injection, Rullst Radar telemetry, and comprehensive documentation.",
+          "timestamp": "2026-08-05T17:00:39-03:00",
+          "tree_id": "36ed9ceb03d69ed3a2b2f6c177bfa8511265da89",
+          "url": "https://github.com/Rullst/Rullst/commit/7004aecbf98363bc245a29379c68e91cb97a925e"
+        },
+        "date": 1785960535336,
         "tool": "cargo",
         "benches": [
           {
