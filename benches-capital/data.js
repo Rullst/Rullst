e2ee18a42811b7bab6a357c0defb587399a152d9
github-action-benchmark <github@users.noreply.github.com>
2026-08-12 18:03:53 +0000

    add Rullst Capital Benchmark (cargo) benchmark result for 2d8255f9e6490e85dca531e87747837d83743106

--- benches-capital/data.js
+++ benches-capital/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1786547672076,
+  "lastUpdate": 1786557833871,
   "repoUrl": "https://github.com/Rullst/Rullst",
   "entries": {
     "Rullst Capital Benchmark": [
@@ -712,6 +712,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "capital_subscription/status_as_str",
             "value": 5,
+            "range": "± 0",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "venelouistyago@gmail.com",
+            "name": "Venelouis",
+            "username": "venelouis"
+          },
+          "committer": {
+            "email": "venelouistyago@gmail.com",
+            "name": "Venelouis",
+            "username": "venelouis"
+          },
+          "distinct": true,
+          "id": "2d8255f9e6490e85dca531e87747837d83743106",
+          "message": "test: remove memory database initialization from server tests and disable studio/nexus tests under strict database features",
+          "timestamp": "2026-08-12T14:52:23-03:00",
+          "tree_id": "a98f524a8f547934a0a76dd58c68509bd6c86975",
+          "url": "https://github.com/Rullst/Rullst/commit/2d8255f9e6490e85dca531e87747837d83743106"
+        },
+        "date": 1786557833481,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "capital_subscription/parse_status_active",
+            "value": 20,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "capital_subscription/parse_status_past_due",
+            "value": 23,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "capital_subscription/status_as_str",
+            "value": 6,
             "range": "± 0",
             "unit": "ns/iter"
           }
