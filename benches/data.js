0e9ee32b2e9760db4dba03ca390472aaaee8be0d
github-action-benchmark <github@users.noreply.github.com>
2026-08-16 06:24:52 +0000

    add Rullst Core Benchmark (cargo) benchmark result for 4db5e073af71f80c6bf535ce2b73dcf5302320dd

--- benches/data.js
+++ benches/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1786858756252,
+  "lastUpdate": 1786861492563,
   "repoUrl": "https://github.com/Rullst/Rullst",
   "entries": {
     "Benchmark": [
@@ -8317,6 +8317,60 @@ window.BENCHMARK_DATA = {
             "name": "waf_middleware_overhead",
             "value": 1589,
             "range": "± 19",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "venelouistyago@gmail.com",
+            "name": "Venelouis",
+            "username": "venelouis"
+          },
+          "committer": {
+            "email": "venelouistyago@gmail.com",
+            "name": "Venelouis",
+            "username": "venelouis"
+          },
+          "distinct": true,
+          "id": "4db5e073af71f80c6bf535ce2b73dcf5302320dd",
+          "message": "feat: re-export axum http primitives in lib.rs for easier access",
+          "timestamp": "2026-08-16T03:22:46-03:00",
+          "tree_id": "ca20103c97089051b4cab993548ce8f06f9c9de8",
+          "url": "https://github.com/Rullst/Rullst/commit/4db5e073af71f80c6bf535ce2b73dcf5302320dd"
+        },
+        "date": 1786861491736,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "router_match_simple",
+            "value": 847,
+            "range": "± 24",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "router_match_nested_params",
+            "value": 1081,
+            "range": "± 31",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "html_macro_static",
+            "value": 7,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "html_macro_dynamic",
+            "value": 651,
+            "range": "± 20",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "waf_middleware_overhead",
+            "value": 2375,
+            "range": "± 62",
             "unit": "ns/iter"
           }
         ]
