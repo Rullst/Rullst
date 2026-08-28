309a2f9a16ef7ddff433a1ca5bea52e91e103cb7
github-action-benchmark <github@users.noreply.github.com>
2026-08-28 01:50:12 +0000

    add Rullst Capital Benchmark (cargo) benchmark result for d2e5580ab99bf2078b7dbc752de9fb7ff0203f79

--- benches-capital/data.js
+++ benches-capital/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1787866534086,
+  "lastUpdate": 1787881811958,
   "repoUrl": "https://github.com/Rullst/Rullst",
   "entries": {
     "Rullst Capital Benchmark": [
@@ -1674,6 +1674,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "capital_subscription/status_as_str",
             "value": 5,
+            "range": "± 0",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "venelouistyago@gmail.com",
+            "name": "venelouis",
+            "username": "venelouis"
+          },
+          "committer": {
+            "email": "venelouistyago@gmail.com",
+            "name": "venelouis",
+            "username": "venelouis"
+          },
+          "distinct": true,
+          "id": "d2e5580ab99bf2078b7dbc752de9fb7ff0203f79",
+          "message": "feat(v12): harden security contracts and LMS scaffold",
+          "timestamp": "2026-08-27T22:35:19-03:00",
+          "tree_id": "fb43353d9afdf868f1926e5c3854df7dc8ef8e8e",
+          "url": "https://github.com/Rullst/Rullst/commit/d2e5580ab99bf2078b7dbc752de9fb7ff0203f79"
+        },
+        "date": 1787881811464,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "capital_subscription/parse_status_active",
+            "value": 21,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "capital_subscription/parse_status_past_due",
+            "value": 23,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "capital_subscription/status_as_str",
+            "value": 6,
             "range": "± 0",
             "unit": "ns/iter"
           }
