946036ed9f36e58d4c1acb3cd0a6c3c9ad390a81
github-action-benchmark <github@users.noreply.github.com>
2026-08-29 08:02:16 +0000

    add Rullst Connect Benchmark (cargo) benchmark result for dbd82756413a7194006988514993ab7ffb9e5001

--- benches-connect/data.js
+++ benches-connect/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1787985808604,
+  "lastUpdate": 1787990536402,
   "repoUrl": "https://github.com/Rullst/Rullst",
   "entries": {
     "Rullst Connect Benchmark": [
@@ -2455,6 +2455,36 @@ window.BENCHMARK_DATA = {
             "name": "github_provider_creation",
             "value": 569,
             "range": "± 7",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "venelouistyago@gmail.com",
+            "name": "Venelouis",
+            "username": "venelouis"
+          },
+          "committer": {
+            "email": "venelouistyago@gmail.com",
+            "name": "Venelouis",
+            "username": "venelouis"
+          },
+          "distinct": true,
+          "id": "dbd82756413a7194006988514993ab7ffb9e5001",
+          "message": "fix(cli): align generated tera dependency",
+          "timestamp": "2026-08-29T04:31:37-03:00",
+          "tree_id": "528314d9f3fffdff3c6bf0669a3759f7b0bfef8d",
+          "url": "https://github.com/Rullst/Rullst/commit/dbd82756413a7194006988514993ab7ffb9e5001"
+        },
+        "date": 1787990535864,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "github_provider_creation",
+            "value": 324,
+            "range": "± 3",
             "unit": "ns/iter"
           }
         ]
