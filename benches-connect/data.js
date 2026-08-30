2de6b3eb8c5469cf4e9b0d6a663d9c07692d93a8
github-action-benchmark <github@users.noreply.github.com>
2026-08-30 16:54:33 +0000

    add Rullst Connect Benchmark (cargo) benchmark result for 891050c3d08da86fd85b492b8d7db9e95d0d83e7

--- benches-connect/data.js
+++ benches-connect/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1788104025328,
+  "lastUpdate": 1788108873827,
   "repoUrl": "https://github.com/Rullst/Rullst",
   "entries": {
     "Rullst Connect Benchmark": [
@@ -2905,6 +2905,36 @@ window.BENCHMARK_DATA = {
             "name": "github_provider_creation",
             "value": 467,
             "range": "± 1",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "venelouistyago@gmail.com",
+            "name": "Venelouis",
+            "username": "venelouis"
+          },
+          "committer": {
+            "email": "venelouistyago@gmail.com",
+            "name": "Venelouis",
+            "username": "venelouis"
+          },
+          "distinct": true,
+          "id": "891050c3d08da86fd85b492b8d7db9e95d0d83e7",
+          "message": "docs(project): explain why to choose rullst",
+          "timestamp": "2026-08-30T13:42:05-03:00",
+          "tree_id": "a035685c59848d521ef568fa293641afdf69be28",
+          "url": "https://github.com/Rullst/Rullst/commit/891050c3d08da86fd85b492b8d7db9e95d0d83e7"
+        },
+        "date": 1788108873458,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "github_provider_creation",
+            "value": 391,
+            "range": "± 4",
             "unit": "ns/iter"
           }
         ]
