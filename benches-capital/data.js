f18494dd2131342ccbc3bfd048288ddf6df8aaf6
github-action-benchmark <github@users.noreply.github.com>
2026-08-30 16:57:40 +0000

    add Rullst Capital Benchmark (cargo) benchmark result for 891050c3d08da86fd85b492b8d7db9e95d0d83e7

--- benches-capital/data.js
+++ benches-capital/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1788104251187,
+  "lastUpdate": 1788109060345,
   "repoUrl": "https://github.com/Rullst/Rullst",
   "entries": {
     "Rullst Capital Benchmark": [
@@ -2430,6 +2430,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "capital_subscription/status_as_str",
             "value": 6,
+            "range": "± 0",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "venelouistyago@gmail.com",
+            "name": "Venelouis",
+            "username": "venelouis"
+          },
+          "committer": {
+            "email": "venelouistyago@gmail.com",
+            "name": "Venelouis",
+            "username": "venelouis"
+          },
+          "distinct": true,
+          "id": "891050c3d08da86fd85b492b8d7db9e95d0d83e7",
+          "message": "docs(project): explain why to choose rullst",
+          "timestamp": "2026-08-30T13:42:05-03:00",
+          "tree_id": "a035685c59848d521ef568fa293641afdf69be28",
+          "url": "https://github.com/Rullst/Rullst/commit/891050c3d08da86fd85b492b8d7db9e95d0d83e7"
+        },
+        "date": 1788109059983,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "capital_subscription/parse_status_active",
+            "value": 16,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "capital_subscription/parse_status_past_due",
+            "value": 18,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "capital_subscription/status_as_str",
+            "value": 5,
             "range": "± 0",
             "unit": "ns/iter"
           }
