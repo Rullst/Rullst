6fe6cdf0ae02d5bce250a575d7a8ee656419e82d
github-action-benchmark <github@users.noreply.github.com>
2026-08-30 19:25:08 +0000

    add Rullst Connect Benchmark (cargo) benchmark result for cd2b10965e831654639dd2a1188a82d3b326ebbe

--- benches-connect/data.js
+++ benches-connect/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1788114803826,
+  "lastUpdate": 1788117908326,
   "repoUrl": "https://github.com/Rullst/Rullst",
   "entries": {
     "Rullst Connect Benchmark": [
@@ -2965,6 +2965,36 @@ window.BENCHMARK_DATA = {
             "name": "github_provider_creation",
             "value": 539,
             "range": "± 5",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "venelouistyago@gmail.com",
+            "name": "Venelouis",
+            "username": "venelouis"
+          },
+          "committer": {
+            "email": "venelouistyago@gmail.com",
+            "name": "Venelouis",
+            "username": "venelouis"
+          },
+          "distinct": true,
+          "id": "cd2b10965e831654639dd2a1188a82d3b326ebbe",
+          "message": "feat(connect): add bounded corporate proxy client",
+          "timestamp": "2026-08-30T16:17:56-03:00",
+          "tree_id": "5dc0818e47fe6a0fcff8469fc5e7b5c95b5ef1a7",
+          "url": "https://github.com/Rullst/Rullst/commit/cd2b10965e831654639dd2a1188a82d3b326ebbe"
+        },
+        "date": 1788117907822,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "github_provider_creation",
+            "value": 315,
+            "range": "± 15",
             "unit": "ns/iter"
           }
         ]
