3f3c2960201fec5a5f20332bb0b76c8ecedd7fee
github-action-benchmark <github@users.noreply.github.com>
2026-08-30 04:05:02 +0000

    add Rullst Auth Benchmark (cargo) benchmark result for 644388ba7ad58f1c391004cfb761e145a6c6317e

--- benches-auth/data.js
+++ benches-auth/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1788061179203,
+  "lastUpdate": 1788062701974,
   "repoUrl": "https://github.com/Rullst/Rullst",
   "entries": {
     "Rullst Auth Benchmark": [
@@ -3948,6 +3948,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "session_crypto/encrypt_session",
             "value": 478,
+            "range": "± 2",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "venelouistyago@gmail.com",
+            "name": "Venelouis",
+            "username": "venelouis"
+          },
+          "committer": {
+            "email": "venelouistyago@gmail.com",
+            "name": "Venelouis",
+            "username": "venelouis"
+          },
+          "distinct": true,
+          "id": "644388ba7ad58f1c391004cfb761e145a6c6317e",
+          "message": "feat(orm): add typed inverse polymorphic relations",
+          "timestamp": "2026-08-30T00:58:04-03:00",
+          "tree_id": "47313c9e9dc05ea34d1e65f6fcb48aebf7cd4b1f",
+          "url": "https://github.com/Rullst/Rullst/commit/644388ba7ad58f1c391004cfb761e145a6c6317e"
+        },
+        "date": 1788062701555,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "session_crypto/encrypt_session",
+            "value": 498,
             "range": "± 2",
             "unit": "ns/iter"
           }
