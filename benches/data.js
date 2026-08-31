bee98121517392e9f197afdc14a5cc8e38f71a87
github-action-benchmark <github@users.noreply.github.com>
2026-08-31 03:18:34 +0000

    add Rullst Core Benchmark (cargo) benchmark result for 2f0cea1b7984e5384776ac2028f000d730e943cc

--- benches/data.js
+++ benches/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1788143566389,
+  "lastUpdate": 1788146314860,
   "repoUrl": "https://github.com/Rullst/Rullst",
   "entries": {
     "Benchmark": [
@@ -11659,6 +11659,60 @@ window.BENCHMARK_DATA = {
             "name": "waf_middleware_overhead",
             "value": 1702,
             "range": "± 65",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "venelouistyago@gmail.com",
+            "name": "Venelouis",
+            "username": "venelouis"
+          },
+          "committer": {
+            "email": "venelouistyago@gmail.com",
+            "name": "Venelouis",
+            "username": "venelouis"
+          },
+          "distinct": true,
+          "id": "2f0cea1b7984e5384776ac2028f000d730e943cc",
+          "message": "fix(ci): allow official nfse error field",
+          "timestamp": "2026-08-31T00:16:34-03:00",
+          "tree_id": "cac16418aec2d845e0f96b66508e21ace5813c6b",
+          "url": "https://github.com/Rullst/Rullst/commit/2f0cea1b7984e5384776ac2028f000d730e943cc"
+        },
+        "date": 1788146313900,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "router_match_simple",
+            "value": 742,
+            "range": "± 16",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "router_match_nested_params",
+            "value": 981,
+            "range": "± 7",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "html_macro_static",
+            "value": 7,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "html_macro_dynamic",
+            "value": 619,
+            "range": "± 5",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "waf_middleware_overhead",
+            "value": 2243,
+            "range": "± 29",
             "unit": "ns/iter"
           }
         ]
