9f1c547980d82466abb90e5c649038f48ee13661
github-action-benchmark <github@users.noreply.github.com>
2026-08-31 14:31:56 +0000

    add Rullst Connect Benchmark (cargo) benchmark result for 40de8fb554e5bb837bc0daf43bc77d7ced2ac26e

--- benches-connect/data.js
+++ benches-connect/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1788179230331,
+  "lastUpdate": 1788186716871,
   "repoUrl": "https://github.com/Rullst/Rullst",
   "entries": {
     "Rullst Connect Benchmark": [
@@ -3533,6 +3533,36 @@ window.BENCHMARK_DATA = {
             "name": "github_provider_creation",
             "value": 498,
             "range": "± 1",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "venelouistyago@gmail.com",
+            "name": "Venelouis",
+            "username": "venelouis"
+          },
+          "committer": {
+            "email": "venelouistyago@gmail.com",
+            "name": "Venelouis",
+            "username": "venelouis"
+          },
+          "distinct": true,
+          "id": "40de8fb554e5bb837bc0daf43bc77d7ced2ac26e",
+          "message": "feat(capital): add shared transactional quotas",
+          "timestamp": "2026-08-31T11:20:53-03:00",
+          "tree_id": "44ae582dc622e45242c2e96f9b297e6696fdf4d2",
+          "url": "https://github.com/Rullst/Rullst/commit/40de8fb554e5bb837bc0daf43bc77d7ced2ac26e"
+        },
+        "date": 1788186716431,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "github_provider_creation",
+            "value": 555,
+            "range": "± 6",
             "unit": "ns/iter"
           }
         ]
