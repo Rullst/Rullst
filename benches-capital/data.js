ace7972e556c30afa380377345731652a4b3d8bf
github-action-benchmark <github@users.noreply.github.com>
2026-08-31 11:51:15 +0000

    add Rullst Capital Benchmark (cargo) benchmark result for 75fd320af2cfb300b9e53eb0fcf16017e90a2579

--- benches-capital/data.js
+++ benches-capital/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1788173728712,
+  "lastUpdate": 1788177075173,
   "repoUrl": "https://github.com/Rullst/Rullst",
   "entries": {
     "Rullst Capital Benchmark": [
@@ -3136,6 +3136,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "capital_subscription/parse_status_past_due",
             "value": 18,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "capital_subscription/status_as_str",
+            "value": 5,
+            "range": "± 0",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "venelouistyago@gmail.com",
+            "name": "Venelouis",
+            "username": "venelouis"
+          },
+          "committer": {
+            "email": "venelouistyago@gmail.com",
+            "name": "Venelouis",
+            "username": "venelouis"
+          },
+          "distinct": true,
+          "id": "75fd320af2cfb300b9e53eb0fcf16017e90a2579",
+          "message": "test(capital): assert receipt email redaction",
+          "timestamp": "2026-08-31T08:29:39-03:00",
+          "tree_id": "7d2072a41103f59b700c884cabcacc9937f749b5",
+          "url": "https://github.com/Rullst/Rullst/commit/75fd320af2cfb300b9e53eb0fcf16017e90a2579"
+        },
+        "date": 1788177074626,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "capital_subscription/parse_status_active",
+            "value": 17,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "capital_subscription/parse_status_past_due",
+            "value": 17,
             "range": "± 0",
             "unit": "ns/iter"
           },
