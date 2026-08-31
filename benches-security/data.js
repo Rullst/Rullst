f598448e2cbddd684a9e6d10aa3ab82ca3d3dba7
github-action-benchmark <github@users.noreply.github.com>
2026-08-31 10:24:18 +0000

    add Rullst Security Benchmark (cargo) benchmark result for ae17f7b19bef8b7fcb2dff18822d4fa707f70bda

--- benches-security/data.js
+++ benches-security/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1788170749909,
+  "lastUpdate": 1788171858081,
   "repoUrl": "https://github.com/Rullst/Rullst",
   "entries": {
     "Rullst Security Benchmark": [
@@ -3985,6 +3985,60 @@ window.BENCHMARK_DATA = {
             "name": "rbac_guard/authorize_owner_or_role",
             "value": 12,
             "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "vault_secret/vault_secret_new_and_drop",
+            "value": 26,
+            "range": "± 0",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "venelouistyago@gmail.com",
+            "name": "Venelouis",
+            "username": "venelouis"
+          },
+          "committer": {
+            "email": "venelouistyago@gmail.com",
+            "name": "Venelouis",
+            "username": "venelouis"
+          },
+          "distinct": true,
+          "id": "ae17f7b19bef8b7fcb2dff18822d4fa707f70bda",
+          "message": "feat(nexus): validate semantic admin forms",
+          "timestamp": "2026-08-31T07:13:16-03:00",
+          "tree_id": "e46c4a906b4add5b5894561a2a0985995119da55",
+          "url": "https://github.com/Rullst/Rullst/commit/ae17f7b19bef8b7fcb2dff18822d4fa707f70bda"
+        },
+        "date": 1788171857334,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "html_sanitizer/sanitize_html_xss",
+            "value": 5900,
+            "range": "± 249",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "html_sanitizer/sanitize_text_escape",
+            "value": 766,
+            "range": "± 13",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "rbac_guard/authorize_role",
+            "value": 14,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "rbac_guard/authorize_owner_or_role",
+            "value": 13,
+            "range": "± 1",
             "unit": "ns/iter"
           },
           {
