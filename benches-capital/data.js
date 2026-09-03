8332c27193ef3a15ca0d9082b2b7a91c1b064ada
github-action-benchmark <github@users.noreply.github.com>
2026-09-03 13:10:44 +0000

    add Rullst Capital Benchmark (cargo) benchmark result for 28e2cea9b8aa63ff01156dfb02c29405464a6c17

--- benches-capital/data.js
+++ benches-capital/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1788428483029,
+  "lastUpdate": 1788441044726,
   "repoUrl": "https://github.com/Rullst/Rullst",
   "entries": {
     "Rullst Capital Benchmark": [
@@ -5368,6 +5368,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "capital_subscription/status_as_str",
             "value": 7,
+            "range": "± 0",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "venelouistyago@gmail.com",
+            "name": "Venelouis",
+            "username": "venelouis"
+          },
+          "committer": {
+            "email": "venelouistyago@gmail.com",
+            "name": "Venelouis",
+            "username": "venelouis"
+          },
+          "distinct": true,
+          "id": "28e2cea9b8aa63ff01156dfb02c29405464a6c17",
+          "message": "feat(orm): add authenticated document recovery",
+          "timestamp": "2026-09-03T09:57:16-03:00",
+          "tree_id": "15481e1f40d4d08ff10f275ba37adf90a9ebe682",
+          "url": "https://github.com/Rullst/Rullst/commit/28e2cea9b8aa63ff01156dfb02c29405464a6c17"
+        },
+        "date": 1788441044216,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "capital_subscription/parse_status_active",
+            "value": 17,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "capital_subscription/parse_status_past_due",
+            "value": 20,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "capital_subscription/status_as_str",
+            "value": 6,
             "range": "± 0",
             "unit": "ns/iter"
           }
