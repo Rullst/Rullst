f00a50998b16877ebcb2f364de307f1780766929
github-action-benchmark <github@users.noreply.github.com>
2026-09-04 20:02:45 +0000

    add Rullst Capital Benchmark (cargo) benchmark result for 9aaad0bf8b9acf93273b883657b204a0a9b8c557

--- benches-capital/data.js
+++ benches-capital/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1788541113482,
+  "lastUpdate": 1788552165727,
   "repoUrl": "https://github.com/Rullst/Rullst",
   "entries": {
     "Rullst Capital Benchmark": [
@@ -6334,6 +6334,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "capital_subscription/status_as_str",
             "value": 7,
+            "range": "± 0",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "venelouistyago@gmail.com",
+            "name": "Venelouis",
+            "username": "venelouis"
+          },
+          "committer": {
+            "email": "venelouistyago@gmail.com",
+            "name": "Venelouis",
+            "username": "venelouis"
+          },
+          "distinct": true,
+          "id": "9aaad0bf8b9acf93273b883657b204a0a9b8c557",
+          "message": "chore(release): prepare 12.0.0-rc.1",
+          "timestamp": "2026-09-04T16:49:50-03:00",
+          "tree_id": "cddcd0bc0eb4a76507f27413bee4c7062d46c24f",
+          "url": "https://github.com/Rullst/Rullst/commit/9aaad0bf8b9acf93273b883657b204a0a9b8c557"
+        },
+        "date": 1788552165318,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "capital_subscription/parse_status_active",
+            "value": 20,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "capital_subscription/parse_status_past_due",
+            "value": 22,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "capital_subscription/status_as_str",
+            "value": 6,
             "range": "± 0",
             "unit": "ns/iter"
           }
