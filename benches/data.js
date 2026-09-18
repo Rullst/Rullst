f10889034ab441de4c1134d513cd5e5ce7ea51c8
github-action-benchmark <github@users.noreply.github.com>
2026-09-18 18:39:55 +0000

    add Rullst Core Benchmark (cargo) benchmark result for 174d31e131ce53db498c75b5eea47276a8528f86

--- benches/data.js
+++ benches/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1789743949608,
+  "lastUpdate": 1789756795615,
   "repoUrl": "https://github.com/Rullst/Rullst",
   "entries": {
     "Benchmark": [
@@ -19429,6 +19429,60 @@ window.BENCHMARK_DATA = {
             "name": "waf_middleware_overhead",
             "value": 2304,
             "range": "± 21",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "37526748+venelouis@users.noreply.github.com",
+            "name": "@venelouis",
+            "username": "venelouis"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "174d31e131ce53db498c75b5eea47276a8528f86",
+          "message": "chore(deps): include all six Dependabot updates in v12.1 (#213)",
+          "timestamp": "2026-09-18T15:28:17-03:00",
+          "tree_id": "a0bae6ac3d50dc29ca31b96af7dbe206431bff2e",
+          "url": "https://github.com/Rullst/Rullst/commit/174d31e131ce53db498c75b5eea47276a8528f86"
+        },
+        "date": 1789756794191,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "router_match_simple",
+            "value": 731,
+            "range": "± 10",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "router_match_nested_params",
+            "value": 986,
+            "range": "± 10",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "html_macro_static",
+            "value": 6,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "html_macro_dynamic",
+            "value": 573,
+            "range": "± 7",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "waf_middleware_overhead",
+            "value": 2233,
+            "range": "± 65",
             "unit": "ns/iter"
           }
         ]
