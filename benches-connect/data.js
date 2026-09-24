ebe5d14e1278756939579c2d8fb73edef4af826b
github-action-benchmark <github@users.noreply.github.com>
2026-09-24 15:19:08 +0000

    add Rullst Connect Benchmark (cargo) benchmark result for a8cb57bf5532dad2d49121726336fe67727ae143

--- benches-connect/data.js
+++ benches-connect/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1790223982551,
+  "lastUpdate": 1790263148303,
   "repoUrl": "https://github.com/Rullst/Rullst",
   "entries": {
     "Rullst Connect Benchmark": [
@@ -7307,6 +7307,36 @@ window.BENCHMARK_DATA = {
             "name": "github_provider_creation",
             "value": 530,
             "range": "± 2",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "37526748+venelouis@users.noreply.github.com",
+            "name": "@venelouis",
+            "username": "venelouis"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "a8cb57bf5532dad2d49121726336fe67727ae143",
+          "message": "test(v13): port stable regressions and verify scope isolation",
+          "timestamp": "2026-09-24T11:58:59-03:00",
+          "tree_id": "dd67bb0d64a2f031273df6c48f60f45713a161a8",
+          "url": "https://github.com/Rullst/Rullst/commit/a8cb57bf5532dad2d49121726336fe67727ae143"
+        },
+        "date": 1790263147384,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "github_provider_creation",
+            "value": 417,
+            "range": "± 9",
             "unit": "ns/iter"
           }
         ]
